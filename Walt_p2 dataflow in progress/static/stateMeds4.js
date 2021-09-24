f3aef688d1c697e740b57a57e085144a665b6eb9
Wdepalma <wdepalma@verizon.net>
2021-09-23 23:32:26 -0400

    finally the plot works..

--- Walt_p2 dataflow in progress/static/stateMeds4.js
+++ Walt_p2 dataflow in progress/static/stateMeds4.js
@@ -3,30 +3,25 @@ var state_names = [];
 var price = [];
 var avg_price = []
 var when = []
+var when_new = " "
+var conv_when = []
 console.log("Hello")
 d3.json("/page3").then(function(data) { 
  var state_h = data 
  console.log(data)
- 
+ window.data = data
  for (var key in state_h) {
    
-      if (state_h[key].states === "alabama") {
+      if (state_h[key].states === "pennsylvania") {
     state_names.push(state_h[key].states);
     price.push(state_h[key].median_listing_price);
     avg_price.push(state_h[key].average_listing_price);
     when.push(state_h[key].month_date_yyyymm)
+    
       }
   }
 
-
-//console.log(state_names);
-console.log(when)
-console.log(price);
-console.log(avg_price);
-
-});
-
-
+ 
 var trace1 = {
   x: when,
   y: price,
@@ -48,13 +43,17 @@ var layout = {
   title:'Historic Median and Average Listing Price',
   width: 800,
   height: 600,
-  yaxis: { autorange: false, range:[100000, 300000] },
+  xaxis: {type: 'category'},
+  yaxis: { autorange: false, range:[100000, 500000] },
 };
 
-var plot_data = [trace1];
+var plot_data = [trace1, trace2];
 Plotly.newPlot("LinePlot", plot_data, layout);
- 
+});
 
+// END OF D3
+console.log("outside d3",when);
+console.log(when.length);
 metros = []
 hots = []
 
@@ -64,17 +63,24 @@ d3.json("/page4").then(function(hotdata) {
   
   for (var key in metro_h) {
     
-       if (metro_h[key].state_id === "tx") {
+       if (metro_h[key].state_id.includes('pa')) {
      metros.push(metro_h[key].metro);
      hots.push(metro_h[key].hotness_score);
      
        }
    }
+  console.log(metros);
+  console.log(hots);
+
+  var bardata = [
+    {
+      x: metros,
+      y: hots,
+      type: 'bar'
+    }
+  ];
+  
+  Plotly.newPlot('barchart', bardata);
  
- 
- //console.log(state_names);
- console.log(metros)
- console.log(hots);
- 
- 
+//END of d3
  });
